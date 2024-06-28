0ebab0de20a4ad9f67dbfc7574880ed5820a6247
Javier Miz <javiermizarevalo@gmail.com>
2024-06-28 03:48:29 -0500

    add header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#468bc2',
+        secondary: '#ea0a72',
+        accent: '#969696',
+      },
+      fontFamily: {
+        sans: ['Averta', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
