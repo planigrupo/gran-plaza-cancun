35768a6db23b45fd38e16ea90c47592a1ee9e833
DanielAbnerBT <danielabnerbt@gmail.com>
2024-07-01 17:59:33 -0600

    stores added in a single file

---
+++ src/data/stores.js
@@ -0,0 +1,29 @@
+import McDonals from "@images/stores/logo_McDonalds.png"
+import HEB from "@images/stores/logo_HEB.png"
+import Coopel from "@images/stores/logo_Coppel.png"
+
+
+export const stores = [
+    {
+      title:"McDonals",
+      src: McDonals,
+      alt: "Store 1",
+      horario: "9:00 AM - 9:00 PM L - V 10:00 AM - 7:30 PM S & D",
+      categoria: "Clothing"
+    },
+    {
+      title:"HEB",
+      src: HEB,
+      alt: "Store 2",
+      horario: "10:00 AM - 8:00 PM",
+      categoria: "Electronics"
+    },
+    {
+      title:"Coopel",
+      src: Coopel,
+      alt: "Store 2",
+      horario: "10:00 AM - 8:00 PM",
+      categoria: "Electronics"
+    }
+  ];
+  
