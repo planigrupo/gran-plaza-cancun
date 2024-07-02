8dbfa7018a41350aaa4f36ea791131a4484565d7
JavierMiz <javiermizarevalo@gmail.com>
2024-07-02 01:40:36 -0500

    chore: Update directory layout and component styles in GaleriaDirectorio.astro

--- src/data/stores.js
+++ src/data/stores.js
@@ -3,23 +3,23 @@ export const stores = [
     title: 'McDonals',
     imagePath: 'logo_McDonalds.png',
     alt: 'Store 1',
-    horario: `<strong>L-V:</strong> 9:00am - 9:00 PM 
+    horario: `<strong>L-V: </strong> 9:00am - 9:00pm 
       <br/> 
-      <strong>S-D:</strong> 10:00am - 7:30pm`,
-    categoria: 'Restaurants',
+      <strong>S-D: </strong> 10:00am - 7:30pm`,
+    categoria: 'Restaurantes',
   },
   {
     title: 'HEB',
     imagePath: 'logo_HEB.png',
     alt: 'Store 2',
     horario: '10:00am - 8:00pm',
-    categoria: 'Electronics',
+    categoria: 'Electronicos',
   },
   {
     title: 'Coopel',
     imagePath: 'logo_Coppel.png',
     alt: 'Store 2',
     horario: '10:00am - 8:00pm',
-    categoria: 'Electronics',
+    categoria: 'Electronicos',
   },
 ];
