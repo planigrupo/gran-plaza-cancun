879266a4fe5c695c391d4a17638ebf6ea217728b
JavierMiz <javiermizarevalo@gmail.com>
2024-07-02 03:25:01 -0500

    refactor: Update GaleriaDirectorio component to include search functionality

--- src/components/sections/GaleriaDirectorio.tsx
+++ src/components/sections/GaleriaDirectorio.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
+import Fuse from 'fuse.js';
 import DirectorioItem from '@components/common/DirectorioItem';
 import type { Store } from 'src/data/stores';
 
@@ -26,15 +27,56 @@ const GaleriaDirectorio: React.FC<Props> = ({ className, stores }) => {
     setSelectedCategory(event.target.value);
   };
 
+  /**
+   * Handles the change event of the search input field.
+   * Updates the search query state with the lowercase value of the input.
+   *
+   * @param event - The change event object.
+   */
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value.toLowerCase());
   };
 
-  const filteredStores = stores.filter(
-    (store) =>
-      (selectedCategory === 'Todos' || store.categoria === selectedCategory) &&
-      store.title.toLowerCase().includes(searchQuery)
-  );
+  const fuseOptions = {
+    includeScore: true,
+    threshold: 0.5,
+    // Ajusta las keys según las propiedades de tus objetos Store
+    keys: ['title'],
+  };
+
+  const fuse = useMemo(() => new Fuse(stores, fuseOptions), [stores]);
+
+  /**
+   * Returns the filtered stores based on the search query and selected category.
+   * If no search query is provided and the selected category is 'Todos', it returns all stores.
+   * Otherwise, it performs a search using Fuse.js library and filters the results based on the selected category.
+   *
+   * @param {string} searchQuery - The search query entered by the user.
+   * @param {string} selectedCategory - The selected category to filter the stores.
+   * @param {Array<Store>} stores - The array of all stores.
+   * @param {Fuse} fuse - The Fuse.js instance used for searching.
+   * @returns {Array<Store>} - The filtered stores based on the search query and selected category.
+   */
+  const filteredStores = useMemo(() => {
+    if (!searchQuery && selectedCategory === 'Todos') {
+      return stores;
+    }
+
+    /**
+     * Searches for stores based on the search query or returns all stores if no query is provided.
+     * @returns An array of search results, where each result contains an item property representing a store.
+     */
+    const searchResults = searchQuery
+      ? fuse.search(searchQuery)
+      : stores.map((store) => ({ item: store }));
+
+    return searchResults
+      .map((result) => result.item)
+      .filter(
+        (store) =>
+          selectedCategory === 'Todos' || store.categoria === selectedCategory
+      );
+  }, [searchQuery, selectedCategory, stores, fuse]);
 
   return (
     <section className={className}>
@@ -60,6 +102,7 @@ const GaleriaDirectorio: React.FC<Props> = ({ className, stores }) => {
             ))}
           </select>
         </div>
+
         <div
           className='grid grid-cols-2 gap-px
                      md:grid-cols-3
