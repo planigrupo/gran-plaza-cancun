7ca59e216202c08764feb9ab7b11b3443b6e48f7
DanielAbnerBT <danielabnerbt@gmail.com>
2024-07-15 10:36:16 -0600

    fix vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import vercel from '@astrojs/vercel/static';
 import react from "@astrojs/react";
 
 // https://astro.build/config
