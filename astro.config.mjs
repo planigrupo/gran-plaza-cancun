870962df40a724f3423833fa4c8d1db2de458ac5
DanielAbnerBT <danielabnerbt@gmail.com>
2024-07-15 11:06:29 -0600

    vercelStatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
+import vercelStatic from '@astrojs/vercel/static';
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'hybrid',
-  adapter: vercel({
+  output: 'static',
+  adapter: vercelStatic({
     imageService: true,
   })
 });
