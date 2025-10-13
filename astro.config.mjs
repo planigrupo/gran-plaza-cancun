ca0abefc38114329083e282370eb6d9743685fe9
dabalderasPlanigrupo <chernandezh@gminfra.mx>
2025-10-13 12:33:16 -0600

    setup templete v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vercelStatic from '@astrojs/vercel/static';
-import react from "@astrojs/react";
 
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: 'static',
-  adapter: vercelStatic({
-    imageService: true,
-  })
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
